e87a24b9f569d9df69ca4d88b37f29dcf8ae2423
Jeffrey Vos <200751@student.glu.nl>
2021-09-16 16:47:28 +0200

    Functionaliteit toegevoegd, Drone styling toegevoegd. Drone beweegt als de user de joystick gebruikt.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('press', msg => {
+    io.emit('sendFromServerKeyPressed', 'DIT KNOPJE IS INGEDRUKT: '+msg);
+  });
+
+  socket.on('touchJoy', position => {
+    io.emit('joystickPosition', position);
+  });
+  socket.on('sliderValue', slider => {
+    io.emit('InputRangeValue', slider);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
---
+++ public/assets/js/script.js
@@ -0,0 +1,18 @@
+let antenna = document.querySelectorAll(".antenna_top");
+let powerbutton = document.querySelector("#powerbutton");
+powerbutton.addEventListener("click", powerOnOff);
+
+function powerOnOff(e){
+   for (i = 0; i < antenna.length; i++){
+       antenna[i].classList.toggle("antenna_active");
+   }
+//    checkPower();
+};
+
+// function checkPower(){
+//     if (antenna[i].classList.contains("antenna_active")){
+//         console.log("Power On")
+//     } else {
+//         console.log("Power Off")
+//     }
+// };
